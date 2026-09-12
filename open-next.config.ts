4f2abf1df0ae1c9fd0422aba2dbe90af7c60c9eb
Darma Cahya <darmady6@gmail.com>
2026-09-12 12:38:18 +0700

    chore: update OpenNext Cloudflare configuration and build scripts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,11 +1,23 @@
 import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
-import cache from "@opennextjs/cloudflare/kv-cache";
 
 const config: OpenNextConfig = {
   default: {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
       incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
